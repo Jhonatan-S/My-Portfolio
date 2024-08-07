4b4bd7704aa9befa07de2492b1ec3b4b4dbcb532
Jhonatan-S <studyprogrammer5@gmail.com>
2024-08-07 20:52:14 -0300

    changes

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -1,34 +1,21 @@
 "use client"
 
 import { bgHome, oswald } from "@/assets"
-import { motion, useTransform, useScroll, MotionValue } from "framer-motion"
-import { useEffect, useRef, useState } from "react"
-
+import { motion, useTransform } from "framer-motion"
+import { useScroll } from "framer-motion"
+import { useRef } from "react"
 export const Hero = () => {
 
     const title = "Welcome"
     const containerRef = useRef(null)
-    const [scrollYProgress, setScrollYProgress] = useState<MotionValue<number> | null>(null)
-    const [yValues, setYValues] = useState<MotionValue<number>[]>([])
 
-    useEffect(() => {
-        if (containerRef.current) {
-            const { scrollYProgress: progress } = useScroll({
-                target: containerRef,
-                offset: ['end center', 'end end']
-            })
-            setScrollYProgress(progress)
-        }
-    }, [])
+    const {scrollYProgress} = useScroll({
+        target: containerRef,
+        offset: ['end center', 'end end']
+    })
 
-    useEffect(() => {
-        if (scrollYProgress) {
-            const yValuesArray = title.split("").map((_, i) => 
-                useTransform(scrollYProgress, [0, 1], [-200 * (i + 1), 0])
-            )
-            setYValues(yValuesArray)
-        }
-    }, [scrollYProgress, title])
+    const y = useTransform(scrollYProgress, [0, 1], [-200, 0])
+
 
     return (
         <section ref={containerRef} className="relative min-h-screen w-full center justify-center px-px before:bg-[#0000007c] before:inset-0 before:z-10 before:absolute">
@@ -42,19 +29,22 @@ export const Hero = () => {
             />
 
             <div className="center min-h-[30rem] relative z-10">
+
                 {title.split("").map((letter, index) => (
+
                     <motion.span
+                    
                         key={index}
                         initial={{ scale: 0 }}
                         animate={{ scale: 1 }}
                         whileHover={{ scale: 1.3 }}
                         transition={{ duration: .5 }}
-                        className={`${oswald.className} text-center relative text-white z-10 font-bold cursor-pointer big-text`}
-                        style={{ y: yValues[index] }}
-                    >
+                        className={`${oswald.className} text-center  relative text-white z-10 font-bold cursor-pointer big-text`} style={{y}}>
                         {letter}
+
                     </motion.span>
                 ))}
+              
             </div>
         </section>
     )
